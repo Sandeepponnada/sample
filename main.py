7d4ad451ca41a2acc9d2395d5a6c78e7066775eb
Sandeepponnada <98252409+Sandeepponnada@users.noreply.github.com>
2023-12-29 14:36:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import streamlit as st
-st.set_page_config(layout="wide")
-st.tittle("student management System")
-st.sidebar.tittle("free managemnet  System")
-st.sidebar.subheader("Add student")
-name=st.sidebar.text_input("name")
-fees= st.sidebar.text_input("amount")
-add=t.sidebar.button("ADD")
+import numpy as np
+
+st.set_page_config(layout ="wide")
+st.title("Student Management System")
+st.sidebar.title("Fee Management System")
+
+st.sidebar.subheader("Add Student")
+rollnumber=st.sidebar.number_input("Roll Number")
+name=st.sidebar.text_input("Name")
+fees=st.sidebar.number_input("Fees")
+add=st.sidebar.button("Add")
