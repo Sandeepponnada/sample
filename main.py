92da4dc69a016ac6ae86dc78f7ee7328bc4b57db
Sandeepponnada <98252409+Sandeepponnada@users.noreply.github.com>
2023-12-29 15:15:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,20 +9,21 @@ st.sidebar.subheader("Menu")
 radio_value = st.sidebar.radio("Choose an option:", ["add student", "delete student", "view fee","pay fee"])
 st.write("You selected:", radio_value)
 if radio_value =="add student":
-  rollnumber=st.number_input("Enter student number")
+  rollnumber=st.text_input("Enter student number")
   name=st.text_input("Name")
   fees=st.number_input("Fees")
   add=st.button("Add Student")
 elif radio_value == "delete student":
-  rollnumber=st.number_input("Enter student number")
+  rollnumber=st.text_input("Enter student number")
   name=st.text_input("enter Name")
   delete=st.button("delete record")
 elif radio_value =="view fee":
-  rollnumber=st.number_input("Enter student number")
+  rollnumber=st.text_input("Enter student number")
   name=st.text_input("enter Name")
   view=st.button("view fee")
 elif radio_value=="pay fee":
-  rollnumber=st.number_input("Enter student number")
+  rollnumber=st.text_input("Enter student number")
   name=st.text_input("Enter Name")
+  fee=st.number_input("enter amount")
   Pay=st.button("Pay")
 
