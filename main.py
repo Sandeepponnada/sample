43bca8ce6f781be791561875ecd5fbc2f2e3ef39
Sandeepponnada <98252409+Sandeepponnada@users.noreply.github.com>
2023-12-29 10:08:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 import streamlit as st
 st.markdown("This is my first App")
 st.write("Welcome to my Sample Streamlit App. This will bring a positive vibes to learn streamlit.It is a python Libary")
+st.write("<h1>Sandeep</h1>",unsafe_allow_html=True)
+#ing colour of test
+st.write("<h1ss style='color:red;'>Murali Ganesh</h1>",unsafe_allow_html=True)
+st.file_uploader("upload file")
+st.image("C:\Users\user\OneDrive\Desktop\mydata\mydata\raw\raw3")
