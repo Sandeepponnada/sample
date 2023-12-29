261510eb4d9ba585162fb9b88310ea0ce6398d47
Sandeepponnada <98252409+Sandeepponnada@users.noreply.github.com>
2023-12-29 14:57:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ st.title("Student Management System")
 st.sidebar.title("Fee Management System")
 
 st.sidebar.subheader("Menu")
-radio_value = st.siderbar.radio("Choose an option:", ["add student", "delete student", "view fee","pay fee"])
-t.write("You selected:", radio_value)
+radio_value = st.radio("Choose an option:", ["add student", "delete student", "view fee","pay fee"])
+st.sidebar.write("You selected:", radio_value)
