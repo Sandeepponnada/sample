4b8f09770c351b56f401350008aacae402756a38
Sandeepponnada <98252409+Sandeepponnada@users.noreply.github.com>
2023-12-29 15:09:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,3 +8,14 @@ st.sidebar.title("Fee Management System")
 st.sidebar.subheader("Menu")
 radio_value = st.sidebar.radio("Choose an option:", ["add student", "delete student", "view fee","pay fee"])
 st.write("You selected:", radio_value)
+if radio_value =="add student":
+  rollnumber=st.number_input("Enter student number")
+  name=st.text_input("Name")
+  fees=st.number_input("Fees")
+  add=st.button("Add Student")
+elif radio_value == "delete student":
+  delete=st.button("delete record")
+elif radio_value =="view fee":
+  view=st.button("view fee")
+elif radio_value=="pay fee":
+  Pay=st.button("Pay")
