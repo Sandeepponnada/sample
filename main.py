491afed983de4902123fa4b948fd01146a78c65b
Sandeepponnada <98252409+Sandeepponnada@users.noreply.github.com>
2023-12-29 14:50:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,5 +6,4 @@ st.title("Student Management System")
 st.sidebar.title("Fee Management System")
 
 st.sidebar.subheader("Menu")
-st.sidebar.subheader("choose an option")
-st.siderbar.radio("Add student","delete student","view fee","pay fee")
+st.siderbar.radio("Choose an option:",["Add student","delete student","view fee","pay fee"])
